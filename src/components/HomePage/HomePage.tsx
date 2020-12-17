92fa2e1945fd549855e8337e9760ce521af0453f
chaitanya rishi <chaitanya167@gmail.com>
2020-12-17 16:10:57 -0500

    remove the alert

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -14,16 +14,19 @@ export const HomePage = () => {
   const [roomName, setRoomName] = useState("");
   const [showError, setShowError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [show, setShow] = useState(false);
 
   const fn = () => {
     var letterNumber = /^[0-9a-zA-Z]+$/;
     console.log(letterNumber.test(roomName));
     if((roomName.length < 4 || roomName.length > 10)) {
       setShowError(true);
+      setShow(true);
       setErrorMessage("Length should be between 4 and 10 characters");
     }
     else if(!letterNumber.test(roomName)) {
       setShowError(true);
+      setShow(true);
       setErrorMessage("Room name should only include letters and numbers");
     }
   } 
@@ -33,31 +36,33 @@ export const HomePage = () => {
   }
 
   return (
-    <Container fluid className="mainContainer">
-      <Row className="justify-content-center logoRow">
-        <Image src={Logo} alt="service logo" className="logoImage" />
-      </Row>
-      <Row className="justify-content-center inputRow">
-        <Container>
-        {showError && 
-        <Alert variant="danger" dismissible>
-          <p>
-            {errorMessage}
-          </p>
-        </Alert>}
-          <Form.Group>
-            <Col sm={{ span: 4, offset: 4 }}>
-              <Form.Control
-                type="text"
-                placeholder="Enter Room Name"
-                className="mt-5"
-                onChange = {(e: React.ChangeEvent<HTMLInputElement>) => handleRoomNameChange(e)}
-              />
-            </Col>
-          </Form.Group>
-          <Button onClick={fn} variant="primary">Submit</Button>
-        </Container>
-      </Row>
-    </Container>
+    
+      <Container fluid className="mainContainer">
+        <Row className="justify-content-center logoRow">
+          <Image src={Logo} alt="service logo" className="logoImage" />
+        </Row>
+        <Row className="justify-content-center inputRow">
+          <Container>
+          {show && showError && 
+          <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+            <p>
+              {errorMessage}
+            </p>
+          </Alert>}
+            <Form.Group>
+              <Col sm={{ span: 4, offset: 4 }}>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Room Name"
+                  className="mt-5"
+                  onChange = {(e: React.ChangeEvent<HTMLInputElement>) => handleRoomNameChange(e)}
+                />
+              </Col>
+            </Form.Group>
+            <Button onClick={fn} variant="primary">Submit</Button>
+          </Container>
+        </Row>
+      </Container>
+    
   );
 };

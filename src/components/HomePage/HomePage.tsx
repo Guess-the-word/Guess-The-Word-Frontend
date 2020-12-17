bc3c1902e85ffc02a03b9f1daba4630057ec5d5d
chaitanya rishi <chaitanya167@gmail.com>
2020-12-16 20:41:53 -0500

    validate Input and error messages

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HomePage.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -7,8 +7,31 @@ import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
+import Alert from 'react-bootstrap/Alert';
 
 export const HomePage = () => {
+
+  const [roomName, setRoomName] = useState("");
+  const [showError, setShowError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fn = () => {
+    var letterNumber = /^[0-9a-zA-Z]+$/;
+    console.log(letterNumber.test(roomName));
+    if((roomName.length < 4 || roomName.length > 10)) {
+      setShowError(true);
+      setErrorMessage("Length should be between 4 and 10 characters");
+    }
+    else if(!letterNumber.test(roomName)) {
+      setShowError(true);
+      setErrorMessage("Room name should only include letters and numbers");
+    }
+  } 
+
+  function handleRoomNameChange(e: React.ChangeEvent<HTMLInputElement>):void{
+    setRoomName(e.target.value);
+  }
+
   return (
     <Container fluid className="mainContainer">
       <Row className="justify-content-center logoRow">
@@ -16,16 +39,23 @@ export const HomePage = () => {
       </Row>
       <Row className="justify-content-center inputRow">
         <Container>
+        {showError && 
+        <Alert variant="danger" dismissible>
+          <p>
+            {errorMessage}
+          </p>
+        </Alert>}
           <Form.Group>
             <Col sm={{ span: 4, offset: 4 }}>
               <Form.Control
                 type="text"
                 placeholder="Enter Room Name"
                 className="mt-5"
+                onChange = {(e: React.ChangeEvent<HTMLInputElement>) => handleRoomNameChange(e)}
               />
             </Col>
           </Form.Group>
-          <Button variant="primary">Submit</Button>
+          <Button onClick={fn} variant="primary">Submit</Button>
         </Container>
       </Row>
     </Container>
